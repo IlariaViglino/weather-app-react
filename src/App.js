3ece3013c4fab128aa3886473d7470d5868633d2
IlariaViglino <ilaria.viglino@yahoo.it>
2024-08-11 21:24:12 +0200

    last details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./styles.css";
 import "./styles.css";
 import Footer from "./Footer.js";
 import Header from "./Header.js";
-import Main from "./Main.js";
+
 import Forecast from "./Forecast.js";
 
 export default function App() {
